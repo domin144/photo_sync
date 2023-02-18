0c61c1e5854658db256a802c001719a78e4f407e
Dominik Wójt <domin144@o2.pl>
2023-02-18 16:44:33 +0100

    Add commandline parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+
+/// Simple program to update target photo collection to have a folder structure
+/// of the source photo collection.
+/// The program tries to avoid copying files and moves files, which match in
+/// file name and size.
+/// The source collection is never modified.
+/// The files in the target collection are never deleted, but may be moved to
+/// new location to match the source structure.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Source collection
+    source_directory: String,
+
+    /// Target collection.
+    target_directory: String,
+
+    /// Don't do anything, just list the actions.
+    dry_run: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!(
+        "Synchronize photo collection from {} to {}.",
+        args.source_directory, args.target_directory
+    )
 }
