97e670464ebc697c99b381f4257025fe49e884ea
Dominik Wójt <domin144@o2.pl>
2023-02-23 20:13:56 +0100

    introduced analyze_directory function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use std::collections::BTreeMap;
+use std::ffi::OsString;
+use std::io;
+use std::path::{Path, PathBuf};
 
 /// Simple program to update target photo collection to have a folder structure
 /// of the source photo collection.
@@ -17,8 +21,66 @@ struct Args {
     target_directory: String,
 
     /// Don't do anything, just list the actions.
+    #[arg(short, long)]
     dry_run: bool,
 }
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct SizeAndName {
+    size: u64,
+    name: OsString,
+}
+
+struct AnalyzedDirectory {
+    map: BTreeMap<SizeAndName, Vec<PathBuf>>,
+}
+
+fn analyze_directory(path: &Path) -> io::Result<AnalyzedDirectory> {
+    let mut result = AnalyzedDirectory {
+        map: BTreeMap::new(),
+    };
+
+    for entry in path.read_dir()? {
+        let entry = entry?;
+        let path = &entry.path();
+        if path.is_dir() {
+            let mut partial_result = analyze_directory(path)?;
+            result.map.append(&mut partial_result.map);
+        } else if path.is_file() {
+            let size = path.metadata()?.len();
+            let name = path
+                .file_name()
+                .ok_or(io::Error::new(
+                    io::ErrorKind::Other,
+                    "path did not end with a file name",
+                ))?
+                .to_owned();
+            let key = SizeAndName { size, name };
+            let entry = result.map.entry(key).or_insert(Vec::new());
+            entry.push(path.clone());
+        }
+    }
+
+    Ok(result)
+}
+
+struct Move {
+    source: PathBuf,
+    target: PathBuf,
+}
+
+struct RemoveDuplicate {
+    duplicate: PathBuf,
+    original: PathBuf,
+}
+
+enum Operatrion {
+    Move(Move),
+    RemoveDuplicate(RemoveDuplicate),
+    RemoveEmptyDirectory(PathBuf),
+}
+
+// fn sync(sourceDir: &AnalyzedDirectory, targetDir: &AnalyzedDirectory) -> Result<(), String> {}
 
 fn main() {
     let args = Args::parse();
